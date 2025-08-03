14fa9a068a9ff9362c3dcd009841cf47f0c932e1
Pavani Pantula <pavanipantula02@gmail.com>
2025-08-03 23:33:20 +0530

    Fix: Contact form now connected to backend

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import axios from "axiosInstance.js";
+import axios from "../axiosInstance";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
